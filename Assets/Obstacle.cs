12bd06b09ed2e108df9db064d9527a235d0a3b35
kabsingh02 <125093543+kabsingh02@users.noreply.github.com>
2024-05-06 21:20:00 -0500

    Final CS4423
    Finished Game for CS4423

--- Assets/Obstacle.cs
+++ Assets/Obstacle.cs
@@ -7,12 +7,15 @@ public class Obstacle : MonoBehaviour
     public float speed;
     public int damage = 1;
 
+
     private void Update() {
         transform.Translate(Vector2.left * speed * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
+
+        
             other.GetComponent<Player>().lives -= damage;
             Debug.Log(other.GetComponent<Player>().lives);
             Destroy(gameObject);
